cfc8aa888c1d32c0fd3b3683301bc1b93dd55569
Administrator <gurdipsira_1@yahoo.co.uk>
2019-06-02 11:45:20 -0700

    rds config

---
+++ main.tf
@@ -0,0 +1,12 @@
+
+
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-mysql"
+  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  database_name           = "mydb"
+  master_username         = "foo"
+  master_password         = "bar"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+}
