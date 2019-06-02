bac61c1eaa56ede5038adf100c28e780bdb8b093
Administrator <gurdipsira_1@yahoo.co.uk>
2019-06-02 11:55:43 -0700

    rds config

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_rds_cluster" "default" {
   database_name           = "mydb"
   master_username         = "foo"
   master_password         = "bar"
-  backup_retention_period = 5
+  backup_retention_period = 6
   preferred_backup_window = "07:00-09:00"
 }
